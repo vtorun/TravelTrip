2d5917fbee8f8df181b985eeaf90c22d19080bc7
vtorun <64329150+vtorun@users.noreply.github.com>
2023-05-09 19:04:41 +0300

    Add project files.

---
+++ TravelTrip/Controllers/TravelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTrip.Controllers
+{
+    public class TravelController : Controller
+    {
+        // GET: Travel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelTrip/Models/Siniflar/Yorumlar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTrip.Models.Siniflar
+{
+    public class Yorumlar
+    {
+        [Key]
+        public int ID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Maill { get; set; }
+        public string Yorum { get; set; }
+        public int BlogID { get; set; }
+    }
+}
