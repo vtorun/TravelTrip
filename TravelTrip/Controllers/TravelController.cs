98e2b622efe5f3ab340267a5b60b9c9c4e2c3ab9
vtorun <64329150+vtorun@users.noreply.github.com>
2023-05-11 21:12:56 +0300

    HomePage

--- TravelTrip/Controllers/TravelController.cs
+++ TravelTrip/Controllers/TravelController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
 
 namespace TravelTrip.Controllers
 {
     public class TravelController : Controller
     {
         // GET: Travel
+        Context context = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = context.Blogs.ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = context.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var degerler = context.Blogs.Where(x => x.ID == 2).ToList();
+            return PartialView(degerler);
         }
     }
 }
