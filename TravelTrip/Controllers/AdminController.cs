34f5de09324875df48c3f66a9298fdd150b1b4d8
vtorun <64329150+vtorun@users.noreply.github.com>
2023-05-15 22:52:33 +0300

    Blog-Add-Update-Delete

---
+++ TravelTrip/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog blog)
+        {
+            context.Blogs.Add(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Blogsil(int id)
+        {
+            var blog = context.Blogs.Find(id);
+            context.Blogs.Remove(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var blog = context.Blogs.Find(id);
+            return View("BlogGetir", blog);
+        }
+        public ActionResult BlogGuncelle(Blog blog)
+        {
+            var blogDegerler = context.Blogs.Find(blog.ID);
+            blogDegerler.Aciklama = blog.Aciklama;
+            blogDegerler.Baslik = blog.Baslik;
+            blogDegerler.Tarih = blog.Tarih;
+            blogDegerler.BlogImage = blog.BlogImage;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
